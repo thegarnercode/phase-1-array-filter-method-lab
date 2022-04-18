f57daf0baba1672bdd9a2686c6dd8c6e683d6ea6
Denisha Garner <thegarnercode@gmail.com>
2022-04-18 01:44:19 -0400

    commit tree 822a3df2b624b1fce03c78c3709611416e5465e9

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+const drivers =['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby',]
+//console.log(drivers)
+function findMatching(drivers) {
+    return drivers.filter['Bobby']
+}  
+
